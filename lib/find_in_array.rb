686d7e7959d7dc4f58b6cfb2a84e347fd081af7e
patrick simmons <hadroncollides@gmail.com>
2021-01-16 17:13:01 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   def find_element_index(array, value_to_find)
      count = 0
   while count < array.length do
-    0
+   value_to_find = 0
     count += 1
   end
 end
